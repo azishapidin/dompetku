6f87d120bf5da568c9d839a872c513e6808ba3f3
Azis Hapidin <azishapidin@gmail.com>
2018-04-24 15:28:33 +0700

    fix error in soft delete

--- app/Model/Account.php
+++ app/Model/Account.php
@@ -3,9 +3,12 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Account extends Model
 {
+    use SoftDeletes;
+
     /**
      * The attributes that are mass assignable.
      *
--- app/Model/Transaction.php
+++ app/Model/Transaction.php
@@ -3,12 +3,9 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Transaction extends Model
 {
-    use SoftDeletes;
-    
     /**
      * The attributes that are mass assignable.
      *
