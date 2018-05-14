d4c17606caaf31e7b9e90fb11e06a05b6f38bf8a
Azis Hapidin <azishapidin@gmail.com>
2018-05-14 18:55:00 +0700

    add attachment model and define relation

---
+++ app/Model/TransactionAttachment.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Model\Transaction;
+
+class TransactionAttachment extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'transaction_attachment';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'transaction_id', 'file_path'
+    ];
+
+    /**
+     * Relation to Transaction Model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class);
+    }
+}
