0065ecaaa7b24eedade596022abdb19795fa2e34
Azis Hapidin <azishapidin@gmail.com>
2018-05-21 14:20:20 +0700

    change excerpt length

--- app/Model/Transaction.php
+++ app/Model/Transaction.php
@@ -69,7 +69,7 @@ class Transaction extends Model
      */
     public function getExcerptAttribute()
     {
-        $length = 25;
+        $length = 20;
         $description = $this->getAttribute('description');
 
         if (strlen($description) <= $length) {
