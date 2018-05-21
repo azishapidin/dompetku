a4c851481635750a530e9a277090e940f5ac2531
Azis Hapidin <azishapidin@gmail.com>
2018-05-21 16:24:35 +0700

    add condition on detail transaction

--- resources/views/transaction/detail.blade.php
+++ resources/views/transaction/detail.blade.php
@@ -3,9 +3,10 @@
     {{ __('Transaction Type') }}: <strong>{{ strtoupper($transaction->type) }}</strong><br>
     {{ __('Amount') }}: <strong>{{ $transaction->formattedAmount }}</strong><br>
     {{ __('Balance') }}: <strong>{{ $transaction->formattedBalance }}</strong><br>
-    {{ __('Description') }}: <strong>{{ $transaction->description }}</strong><br>
-
-    -----------------------<Br>
+    {{ __('Description') }}:<br><strong>{{ $transaction->description }}</strong><br>
+    @if($transaction->attachment->count() > 0)
+    -----------------------<br>
+    @endif
     @foreach($transaction->attachment as $file)
     <a href="{{ $file->url }}" target="_blank">{{ __('Attachment') }} <span class="fa fa-external-link"></span></a><br>
     @endforeach
